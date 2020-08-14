536bf9f66864f470beb9aad5452c99f00899973c
Simon Tang <SIMON@Simons-MacBook-Pro-4.local>
2020-08-14 12:11:06 -0400

    gitignored user/pass and created config template

--- autologin.sh
+++ autologin.sh
@@ -3,6 +3,7 @@
 
 open -a "Google Chrome" "https://localhost:5000/"
 sleep 3.5
-cliclick -w 10 t:stzn9753 kp:tab t:97537776Heartfilia kp:enter
+source .env
+cliclick -w 10 t:$USERNAME kp:tab t:$PASSWORD kp:enter
 sleep 2
 exit 0
--- startserver.sh
+++ startserver.sh
@@ -4,5 +4,4 @@ cd clientportal.gw
 bin/run.sh root/conf.yaml &
 cd ..
 ./autologin.sh
-cliclick -m verbose -w 1 kd:cmd t:t ku:cmd
 exit 0
